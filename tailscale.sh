b827935a4be2df79dfa4efee0620ed043224b2f6
gfreemoon <deniskazet@gmail.com>
2025-05-15 02:50:28 +0300

    Update tailscale.sh

--- tailscale.sh
+++ tailscale.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-# Установка необходимых пакетов
+### Установка необходимых пакетов
 opkg update
 opkg remove kmod-ipt-nat
 opkg install kmod-nf-nat tailscale tailscaled
 
-# Создание интерфейса tailscale через UCI
+### Создание интерфейса tailscale через UCI
 uci set network.tail=interface
 uci set network.tail.ifname='tailscale0'
 uci set network.tail.proto='none'
 uci commit network
 /etc/init.d/network reload
 
-# Включение и запуск tailscale
+### Включение и запуск tailscale
 /etc/init.d/tailscale enable
 /etc/init.d/tailscale start
 tailscale up
 
-# Настройка фаервола
+### Настройка фаервола
 curl -fsSL https://raw.githubusercontent.com/ilnur1111/routerr/main/tailscale.sh | sh
