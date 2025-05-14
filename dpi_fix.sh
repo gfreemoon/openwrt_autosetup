941ab8453df831b5ff5a5ec0314f6e3fbfbe43c0
gfreemoon <deniskazet@gmail.com>
2025-05-15 02:54:09 +0300

    Update dpi_fix.sh

--- dpi_fix.sh
+++ dpi_fix.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
+
+### Фикс DPI-дурилок в OpenWRT (Zapret, YouTubeUnblock) с Hardware/Software Offloading
 sed -i 's/meta l4proto { tcp, udp } flow offload @ft;/meta l4proto { tcp, udp } ct original packets ge 30 flow offload @ft;/g' /usr/share/firewall4/templates/ruleset.uc
 fw4 restart
+
+### Отключение QUIC на роутере (решает проблемы с видеопотоком, dpi обходами)
+uci add firewall rule
+uci set firewall.@rule[-1].name='Block_UDP_80'
+uci set firewall.@rule[-1].src='*'
+uci set firewall.@rule[-1].dest_port='80'
+uci set firewall.@rule[-1].proto='udp'
+uci set firewall.@rule[-1].target='REJECT'
+
+uci add firewall rule
+uci set firewall.@rule[-1].name='Block_UDP_443'
+uci set firewall.@rule[-1].src='*'
+uci set firewall.@rule[-1].dest_port='443'
+uci set firewall.@rule[-1].proto='udp'
+uci set firewall.@rule[-1].target='REJECT'
+
+uci commit firewall
+/etc/init.d/firewall restart
